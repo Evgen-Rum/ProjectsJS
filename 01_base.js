fd52b8ff781b740f9fc248041d5c0f2ff309c120
Evgen-Rum <new.twunk@mail.ru>
2022-04-19 15:43:20 +0300

    minor fixes

--- 01_base.js
+++ 01_base.js
@@ -1,12 +1,12 @@
-//1 variables
-const firstName = 'Evgenii'
-//const lastName = 'Rumiantsev' //string
-let age = 23 //number
-const isProgrammer = true // boolean
-
-const _ = 'private'
-let $ = 'some value'
-const withNum5 = '5'
+// //1 variables
+// const firstName = 'Evgenii'
+// //const lastName = 'Rumiantsev' //string
+// let age = 23 //number
+// const isProgrammer = true // boolean
+//
+// const _ = 'private'
+// let $ = 'some value'
+// const withNum5 = '5'
 
 //2 Mutation
 //console.log('The person\'s name is ' + firstName + ', and the person\'s age is ' + age)
