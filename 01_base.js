72a1a1fb688b741f2da7f922b58dba08531d8bcc
Evgen-Rum <new.twunk@mail.ru>
2022-04-19 15:41:37 +0300

    getting to know the operators

--- 01_base.js
+++ 01_base.js
@@ -12,7 +12,35 @@ const withNum5 = '5'
 //console.log('The person\'s name is ' + firstName + ', and the person\'s age is ' + age)
 //alert('The person\'s name is ' + firstName + lastName + ', and the person\'s age is ' + age) //browser only
 
-const lastName = prompt('Please enter last name!')
+//const lastName = prompt('Please enter last name!')
 //console.log(firstName + ' ' + lastName + ' ' + age)
-alert(firstName + ' ' + lastName)
+//alert(firstName + ' ' + lastName)
+
+//3 operators
+const currentYear = 2022
+const birthYear = 1999
+
+const age = currentYear - birthYear
+console.log(age)
+
+let a = 10
+const b = 5
+
+let c = 32
+c += a
+console.log(c)
+c -= a
+console.log(c)
+c *= a
+console.log(c)
+c /= a
+console.log(c)
+
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a++)
+console.log(a)
+console.log(--a)
 
