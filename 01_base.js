a6ef18ffc2c609092608fe566b2d2c3dda325e6b
Evgen-Rum <new.twunk@mail.ru>
2022-04-19 15:31:16 +0300

    introduction to mutation and user interaction

--- 01_base.js
+++ 01_base.js
@@ -1,15 +1,18 @@
 //1 variables
 const firstName = 'Evgenii'
-const lastName = 'Rumiantsev' //string
+//const lastName = 'Rumiantsev' //string
 let age = 23 //number
-const isProgramer = true // boolean
+const isProgrammer = true // boolean
 
 const _ = 'private'
 let $ = 'some value'
 const withNum5 = '5'
 
 //2 Mutation
-console.log('The person\'s name is ' + firstName + ', and the person\'s age is ' + age)
-console.log('The person\'s name is ' + firstName + ', and the person\'s age is ' + age)
+//console.log('The person\'s name is ' + firstName + ', and the person\'s age is ' + age)
+//alert('The person\'s name is ' + firstName + lastName + ', and the person\'s age is ' + age) //browser only
 
+const lastName = prompt('Please enter last name!')
+//console.log(firstName + ' ' + lastName + ' ' + age)
+alert(firstName + ' ' + lastName)
 
