f0fe2127a6f5bad038f0453e0ac752b10b598d6a
Evgen-Rum <new.twunk@mail.ru>
2022-04-20 16:13:55 +0300

    interesting string features

--- 03_string.js
+++ 03_string.js
@@ -1,18 +1,52 @@
 const name = 'Evgenii'
 const age = 23
 
-function getAge() {
-    return age
+// function getAge() {
+//     return age
+// }
+//
+// const output = 'Hi, my name is ' + name + ' and my age is ' + age + ' years old'
+// const output1 = `Hi my name is ${name} and my age is ${getAge()} years old`
+// const output2 = `Hi my name is ${name} and my age is ${getAge() < 20 ? 'A' : 'B'} years old`
+// console.log(output)
+// console.log(output1)
+// console.log(output2)
+//
+// const output3 = `
+// <div>This is div</div>
+// <p>This is p</p>`
+// console.log(output3)
+
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.toUpperCase())
+// console.log(name.charAt(2))
+// console.log(name.indexOf('nii'))
+// console.log(name.startsWith('evg'))
+// console.log(name.toLowerCase().startsWith('evg'))
+// console.log(name.endsWith('nii'))
+// console.log(name.repeat(3))
+//
+// const pass = '   password   '
+// console.log(pass)
+// console.log(pass.trim())
+// console.log(pass.trimStart())
+// console.log(pass.trimEnd())
+
+function logPerson(arr, name, age) {
+    // console.log(arr, name, age)
+    if (age < 0) {
+       age = `Incorrect age value! Man ${name} has not yet been born`
+    }
+    return `${arr[0]}${name}${arr[1]}${age}${arr[2]}`
 }
 
-const output = 'Hi, my name is ' + name + ' and my age is ' + age + ' years old'
-const output1 = `Hi my name is ${name} and my age is ${getAge()} years old`
-const output2 = `Hi my name is ${name} and my age is ${getAge() < 20 ? 'A' : 'B'} years old`
+const personName = 'Evgenii'
+const personAge = 23
+const output = logPerson`Name: ${personName}, age: ${personAge} !`
 console.log(output)
-console.log(output1)
-console.log(output2)
 
-const output3 = `
-<div>This is div</div>
-<p>This is p</p>`
-console.log(output3)
+const personName1 = 'Victor'
+const personAge1 = -1
+const output1 = logPerson`Name: ${personName1}, age: ${personAge1} !`
+console.log(output1)
