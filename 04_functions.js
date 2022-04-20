65148b4a495323c4a8f1e3d9d6b00c72d00f061b
Evgen-Rum <new.twunk@mail.ru>
2022-04-20 19:05:24 +0300

    arrow functions features

--- 04_functions.js
+++ 04_functions.js
@@ -1,32 +1,52 @@
 
 // 1 function declaration
 
-greet('Evgenii') // true
-function greet(name) {
-    console.log(`Hello ${name} !`)
-}
-greet('Evgenii') // true
+// greet('Evgenii') // true
+// function greet(name) {
+//     console.log(`Hello ${name} !`)
+// }
+// greet('Evgenii') // true
+//
+// // 2 function expression
+//
+// // greet1('Evgenii') //err
+// const greet1 = function greet1(name) {
+//     console.log('Hello', name, '!')
+// }
+// greet1('Evgenii') // true
+//
+// console.dir(greet)
+//
+// // 3 anonymous functions
+//
+// let counter = 0
+// const interval = setInterval(function () {
+//     if (counter === 5) {
+//         clearInterval(interval)
+//     } else {
+//         console.log(++counter)
+//     }
+// }, 1000)
 
-// 2 function expression
+// 4 arrow functions
 
-greet1('Evgenii') //err
-const greet1 = function greet1(name) {
+function greet(name) {    // f declaration
     console.log('Hello', name, '!')
 }
-greet1('Evgenii') // true
+greet('Evgenii')
 
-console.dir(greet)
+const arrow = (name) => {   // f expression
+    console.log('Hello ' + name + ' !')
+}
+arrow('Victor')
 
-// 3 anonymous functions
+const arrow2 = name => console.log(`Hello ${name} !`)   // f expression
+arrow2('Tatyana')
 
-let counter = 0
-const interval = setInterval(function () {
-    if (counter === 5) {
-        clearInterval(interval)
-    } else {
-        console.log(++counter)
-    }
-}, 1000)
+// greet = arrow = arrow2
+
+const pow2 = num => console.log(num ** 2)   // f expression
+pow2(5)
 
 
 
