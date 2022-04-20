7970015852bdee6a14d99798b5f7d5d4b454547b
Evgen-Rum <new.twunk@mail.ru>
2022-04-20 18:48:37 +0300

    minor fixes

--- 04_functions.js
+++ 04_functions.js
@@ -5,17 +5,17 @@ greet('Evgenii') // true
 function greet(name) {
     console.log(`Hello ${name} !`)
 }
-// greet('Evgenii') // true
+greet('Evgenii') // true
 
 // 2 function expression
 
-// greet1('Evgenii') //err
+greet1('Evgenii') //err
 const greet1 = function greet1(name) {
     console.log('Hello', name, '!')
 }
 greet1('Evgenii') // true
 
-// console.dir(greet)
+console.dir(greet)
 
 // 3 anonymous functions
 
