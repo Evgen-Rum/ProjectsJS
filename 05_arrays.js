ddc182fabc72a8c3615fb13ec193e0694b87042e
Evgen-Rum <new.twunk@mail.ru>
2022-04-23 18:43:11 +0300

    features of methods

--- 05_arrays.js
+++ 05_arrays.js
@@ -86,8 +86,9 @@ const fib = [1, 1, 2, 3, 5, 8, 13]
 // console.log(fib)
 
 const pow2 = num => num ** 2
+const sqrt = num => Math.sqrt(num)
 
-const pow2Fib = fib.map(pow2)
+const pow2Fib = fib.map(pow2).map(sqrt)
 console.log(pow2Fib)
 
 // Task 1
