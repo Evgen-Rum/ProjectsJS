58b7fa4d9f7b2c4f76e884aecd00446cc528ae05
Evgen-Rum <new.twunk@mail.ru>
2022-04-23 20:24:48 +0300

    object creation

---
+++ 06_objects.js
@@ -0,0 +1,10 @@
+const person = {
+    firstName: 'Evgenii',
+    lastName: 'Rumiantsev',
+    age: 23,
+    isProgrammer: true,
+    languages: ['ru', 'en', 'by'],
+    greet() {
+        console.log('Hi all!')
+    }
+}
