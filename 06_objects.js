5533d0385c35b0656ff776471b1b15f8f87b2ec6
Evgen-Rum <new.twunk@mail.ru>
2022-04-24 11:50:07 +0300

    objects features

--- 06_objects.js
+++ 06_objects.js
@@ -10,6 +10,7 @@ const person = {
         console.log('Hi all!')
     }
 }
+console.log(person)
 
 // console.log(person)
 // console.log(person.firstName)
@@ -21,12 +22,30 @@ const person = {
 // person.greet()
 
 delete person['key_3']
-console.log(person)
 
 // const fName = person.firstName
 // const age = person.age
 // const languages = person.languages
 
-const {firstName, age, languages} = person
+// const {firstName, age, languages} = person
+// console.log(firstName, age, languages)
 
+const {firstName, age, languages} = person
 console.log(firstName, age, languages)
+
+const person1 = {
+    firstName1: 'Maks',
+    lastName1: 'Ivanov',
+    age1: undefined,
+    isProgrammer1: false,
+    languages1: ['ru', 'by'],
+    'complex key1': 'Complex value',
+    ['key_' + (1 + 3)]: 'Computed key',
+    greet1() {
+        console.log('Hi all!')
+    }
+}
+
+const {firstName1, age1: personAge1 = 10, languages1} = person1
+console.log(firstName1, personAge1, languages1)
+console.log(person1)
