3aa5702c948a76765ce70cc17368c39ff2ff8da1
Evgen-Rum <new.twunk@mail.ru>
2022-04-24 11:24:50 +0300

    object features

--- 06_objects.js
+++ 06_objects.js
@@ -4,10 +4,16 @@ const person = {
     age: 23,
     isProgrammer: true,
     languages: ['ru', 'en', 'by'],
+    'complex key': 'Complex value',
+    ['key_' + (1 + 2)]: 'Computed key',
     greet() {
         console.log('Hi all!')
     }
 }
 
+console.log(person)
 console.log(person.firstName)
 console.log(person['age'])
+console.log(person['complex key'])
+console.log(person['key_' + (1 + 2)])
+person.greet()
