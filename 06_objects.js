e4aea49814cec579ae5445fcaee0ad963cdc029f
Evgen-Rum <new.twunk@mail.ru>
2022-04-24 13:16:23 +0300

    objects features

--- 06_objects.js
+++ 06_objects.js
@@ -1,16 +1,16 @@
-const person = {
-    firstName: 'Evgenii',
-    lastName: 'Rumiantsev',
-    age: 23,
-    isProgrammer: true,
-    languages: ['ru', 'en', 'by'],
-    'complex key': 'Complex value',
-    ['key_' + (1 + 2)]: 'Computed key',
-    greet() {
-        console.log('Hi all!')
-    }
-}
-console.log(person)
+// const person = {
+//     firstName: 'Evgenii',
+//     lastName: 'Rumiantsev',
+//     age: 23,
+//     isProgrammer: true,
+//     languages: ['ru', 'en', 'by'],
+//     'complex key': 'Complex value',
+//     ['key_' + (1 + 2)]: 'Computed key',
+//     greet() {
+//         console.log('Hi all!')
+//     }
+// }
+// console.log(person)
 
 // console.log(person)
 // console.log(person.firstName)
@@ -21,7 +21,7 @@ console.log(person)
 // console.log(person['key_3'])
 // person.greet()
 
-delete person['key_3']
+// delete person['key_3']
 
 // const fName = person.firstName
 // const age = person.age
@@ -30,22 +30,23 @@ delete person['key_3']
 // const {firstName, age, languages} = person
 // console.log(firstName, age, languages)
 
-const {firstName, age, languages} = person
-console.log(firstName, age, languages)
+// const {firstName, age, languages} = person
+// console.log(firstName, age, languages)
+//
+// const person1 = {
+//     firstName1: 'Maks',
+//     lastName1: 'Ivanov',
+//     age1: undefined,
+//     isProgrammer1: false,
+//     languages1: ['ru', 'by'],
+//     'complex key1': 'Complex value',
+//     ['key_' + (1 + 3)]: 'Computed key',
+//     greet1() {
+//         console.log('Hi all!')
+//     }
+// }
+//
+// const {firstName1, age1: personAge1 = 10, languages1} = person1
+// console.log(firstName1, personAge1, languages1)
+// console.log(person1)
 
-const person1 = {
-    firstName1: 'Maks',
-    lastName1: 'Ivanov',
-    age1: undefined,
-    isProgrammer1: false,
-    languages1: ['ru', 'by'],
-    'complex key1': 'Complex value',
-    ['key_' + (1 + 3)]: 'Computed key',
-    greet1() {
-        console.log('Hi all!')
-    }
-}
-
-const {firstName1, age1: personAge1 = 10, languages1} = person1
-console.log(firstName1, personAge1, languages1)
-console.log(person1)
