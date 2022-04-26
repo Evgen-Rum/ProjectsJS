98eec503e3ab11cc176a648ba64fc56d76cea242
Evgen-Rum <new.twunk@mail.ru>
2022-04-26 19:22:03 +0300

    features of asynchronous functions

--- 07_asynchrony.js
+++ 07_asynchrony.js
@@ -10,3 +10,18 @@
 //     console.log('Interval')
 // }, 2000)
 
+// const delay = (callback, wait = 1000) => {
+//     setTimeout(callback, wait)
+// }
+//
+// delay(() => {
+//     console.log('After 2 seconds')
+// }, 2000)
+
+const delay = (wait = 1000) => {
+        new Promise(() => {
+            setTimeout(() => {
+
+            }, wait)
+        })
+}
