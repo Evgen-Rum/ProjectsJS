4cec7aca52cb1faef51a0c945fe1780ab47674d4
Evgen-Rum <new.twunk@mail.ru>
2022-04-27 14:33:25 +0300

    minor fixes

--- 07_asynchrony.js
+++ 07_asynchrony.js
@@ -18,15 +18,15 @@
 //     console.log('After 2 seconds')
 // }, 2000)
 
-const delay = (wait = 1000) => {
-        const promise = new Promise((resolve, reject) => {
-            setTimeout(() => {
-            // resolve()
-                reject('Something went wrong! Try Again!')
-            }, wait)
-        })
-    return promise
-}
+// const delay = (wait = 1000) => {
+//         const promise = new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//             // resolve()
+//                 reject('Something went wrong! Try Again!')
+//             }, wait)
+//         })
+//     return promise
+// }
 
 // delay(2500).then(() => {
 //     console.log('After 2 second')
@@ -53,19 +53,19 @@ const delay = (wait = 1000) => {
 //     console.log('Finally')
 // })
 
-const getData = () => new Promise(resolve => resolve([1, 1, 2, 3, 5, 8, 13]))
-// getData().then(data => console.log(data))
-
-async function asyncExample() {
-    try {
-        await delay(3000)
-        const data = await getData()
-        console.log('Data: ', data)
-    } catch (e) {
-        console.log(e)
-    } finally {
-        console.log('Finally')
-    }
-}
-
-asyncExample()
+// const getData = () => new Promise(resolve => resolve([1, 1, 2, 3, 5, 8, 13]))
+// // getData().then(data => console.log(data))
+//
+// async function asyncExample() {
+//     try {
+//         await delay(3000)
+//         const data = await getData()
+//         console.log('Data: ', data)
+//     } catch (e) {
+//         console.log(e)
+//     } finally {
+//         console.log('Finally')
+//     }
+// }
+//
+// asyncExample()
