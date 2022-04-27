057cd9a80d85f934df23904288ade35295037ef7
Evgen-Rum <new.twunk@mail.ru>
2022-04-27 14:29:34 +0300

    features of asynchronous functions

--- 07_asynchrony.js
+++ 07_asynchrony.js
@@ -18,15 +18,16 @@
 //     console.log('After 2 seconds')
 // }, 2000)
 
-// const delay = (wait = 1000) => {
-//         const promise = new Promise((resolve, reject) => {
-//             setTimeout(() => {
-//             resolve()
-//             }, wait)
-//         })
-//     return promise
-// }
-//
+const delay = (wait = 1000) => {
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+            // resolve()
+                reject('Something went wrong! Try Again!')
+            }, wait)
+        })
+    return promise
+}
+
 // delay(2500).then(() => {
 //     console.log('After 2 second')
 // }).catch(err => {
@@ -35,19 +36,36 @@
 //         console.log('Finally')
 // })
 
-const delay = (wait = 1000) => {
-    const promise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            reject('Something went wrong! Try Again!')
-        }, wait)
-    })
-    return promise
+// const delay = (wait = 1000) => {
+//     const promise = new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             reject('Something went wrong! Try Again!')
+//         }, wait)
+//     })
+//     return promise
+// }
+//
+// delay(2500).then(() => {
+//     console.log('After 2 second')
+// }).catch(err => {
+//     console.error('Error: ', err)
+// }).finally( () => {
+//     console.log('Finally')
+// })
+
+const getData = () => new Promise(resolve => resolve([1, 1, 2, 3, 5, 8, 13]))
+// getData().then(data => console.log(data))
+
+async function asyncExample() {
+    try {
+        await delay(3000)
+        const data = await getData()
+        console.log('Data: ', data)
+    } catch (e) {
+        console.log(e)
+    } finally {
+        console.log('Finally')
+    }
 }
 
-delay(2500).then(() => {
-    console.log('After 2 second')
-}).catch(err => {
-    console.error('Error: ', err)
-}).finally( () => {
-    console.log('Finally')
-})
+asyncExample()
