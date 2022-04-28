d356bb31289309564becd91a4e96bdc3efef82d9
Evgen-Rum <new.twunk@mail.ru>
2022-04-28 13:47:53 +0300

    Study of interaction with DOM

--- 08_DOM.js
+++ 08_DOM.js
@@ -5,15 +5,15 @@ const heading = document.getElementById('hello')
 // const heading2 = document.querySelector('.h2-class')
 const heading2 = document.querySelector('#sub-hello') // .querySelector - only 1 element
 
-console.dir(heading2)
+// console.dir(heading2)
 
 // const heading3 = heading2.nextElementSibling
 
 const h2List = document.querySelectorAll('h2')
-console.log(h2List)
+// console.log(h2List)
 // const heading3 = h2List[1]
 const heading3 = h2List[h2List.length - 1]
-console.log(heading3)
+// console.log(heading3)
 
 // console.log(heading)
 
@@ -26,8 +26,11 @@ setTimeout(() => {
 }, 3000)
 
 const link = heading3.querySelector('a')
-link.addEventListener('click', () =>{
-    console.log('Click on link')
+link.addEventListener('click', (event) =>{
+    event.preventDefault()
+    console.log('Click on link', event.target.getAttribute('href'))
+    const url = event.target.getAttribute('href')
+    window.location = url
 })
 
 setTimeout(() => {
