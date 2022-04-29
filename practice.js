900a94954eadebf7b2864c35656a5bd1bd209a20
Evgen-Rum <new.twunk@mail.ru>
2022-04-29 16:44:36 +0300

    first task from codewars

---
+++ practice.js
@@ -0,0 +1,15 @@
+// Tasks from codewars
+
+// Task1 - count the number of vowels in a string
+// function getCount(str) {
+//     let vowelsCount = 0;
+//     let vowelsList = 'aeiou'
+//     for (let x = 0; x <= str.length; x++) {
+//         if (vowelsList.indexOf(str[x]) !== -1) {
+//             vowelsCount++
+//         }
+//     }
+//     return vowelsCount;
+// }
+//
+// console.log(getCount('fa del   igoku'))
