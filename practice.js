679c5d4eb9131045cc2d56a0cf5e6637db70f1fa
Evgen-Rum <new.twunk@mail.ru>
2022-04-29 18:21:46 +0300

    Task3

--- practice.js
+++ practice.js
@@ -15,13 +15,26 @@
 // console.log(getCount('fa del   igoku'))
 
 // Task2 - find the maximum and minimum values in a string of numbers
+// const numbers = "1 9 3 4 -5"
+//     function highAndLow(numbers){
+//         numbers = numbers.split(' ').map(Number)
+//         return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers)
+//     // const arr = str.split(' ')
+//     // return console.log(`Min: ${Math.min(...arr)}, Max: ${Math.max(...arr)}`)
+// }
+//
+// console.log(highAndLow(numbers))
 
-const numbers = "1 9 3 4 -5"
-    function highAndLow(numbers){
-        numbers = numbers.split(' ').map(Number)
-        return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers)
-    // const arr = str.split(' ')
-    // return console.log(`Min: ${Math.min(...arr)}, Max: ${Math.max(...arr)}`)
+/* Task3 - Take 2 strings s1 and s2 including only
+letters from ato z. Return a new sorted string, the longest possible,
+containing distinct letters - each taken only once - coming from s1 or s2
+ */
+
+const s1 = 'xyaabbbccccdefww'
+const s2 = 'xxxxyyyyabklmopq'
+
+function longest(s1, s2) {
+    return Array.from(new Set(s1 + s2)).sort().join('')
 }
 
-console.log(highAndLow(numbers))
+console.log(longest(s1, s2))
