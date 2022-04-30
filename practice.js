5e037320e22d98833b81502ccc28905efc5c1724
Evgen-Rum <new.twunk@mail.ru>
2022-04-30 17:22:24 +0300

    Task5

--- practice.js
+++ practice.js
@@ -52,3 +52,18 @@ containing distinct letters - each taken only once - coming from s1 or s2
 //
 // console.log(binaryArrayToNumber(binaryArr))
 
+/* Task5
+Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems.
+It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+Ribonucleic acid, RNA, is the primary messenger molecule in cells.
+RNA differs slightly from DNA its chemical structure and contains no Thymine.
+In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+Create a function which translates a given DNA string into RNA.
+ */
+const dna = 'TUUUTUT'
+function DNAtoRNA(dna) {
+    const result = dna.replace(/T/g, 'U')
+    return result
+}
+
+console.log(DNAtoRNA(dna))
