eafe6040d7724c0f52faa387950e56f00d52b712
Evgen-Rum <new.twunk@mail.ru>
2022-05-11 19:40:22 +0300

    Task 21

--- practice.js
+++ practice.js
@@ -255,3 +255,12 @@ The output should be two capital letters with a dot separating them.
 // }
 //
 // console.log(abbrevName('evgen rum'))
+
+/* Task 21
+Implement a function which convert the given boolean value into its string representation.
+Note: Only valid inputs will be given.
+ */
+
+function booleanToString(b) {
+    return String(b)
+}
