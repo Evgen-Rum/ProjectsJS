ddd5d91e2ef9694736fc41c4bd0ce531e4fb8187
Evgen-Rum <new.twunk@mail.ru>
2022-05-11 19:53:07 +0300

    Task 22

--- practice.js
+++ practice.js
@@ -260,7 +260,25 @@ The output should be two capital letters with a dot separating them.
 Implement a function which convert the given boolean value into its string representation.
 Note: Only valid inputs will be given.
  */
+// function booleanToString(b) {
+//     return String(b)
+// }
 
-function booleanToString(b) {
-    return String(b)
+/* Task 22
+You get an array of numbers, return the sum of all of the positives ones.
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+Note: if there is nothing to sum, the sum is default to 0.
+ */
+function positiveSum(arr) {
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            sum += arr[i]
+        }
+    }
+    return sum
 }
+
+const positiveSum1 = (arr) => arr.reduce((sum, n) => n > 0 ? sum + n : sum, 0)
+
+console.log(positiveSum([1, -2, 3, 4, 5]))
