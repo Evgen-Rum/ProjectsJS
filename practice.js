353dce58186fb8c5f2a709608089c49e7430febf
Evgen-Rum <new.twunk@mail.ru>
2022-05-11 19:55:27 +0300

    Task 22

--- practice.js
+++ practice.js
@@ -269,16 +269,25 @@ You get an array of numbers, return the sum of all of the positives ones.
 Example [1,-4,7,12] => 1 + 7 + 12 = 20
 Note: if there is nothing to sum, the sum is default to 0.
  */
-function positiveSum(arr) {
-    let sum = 0
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > 0) {
-            sum += arr[i]
-        }
-    }
-    return sum
+// function positiveSum(arr) {
+//     let sum = 0
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] > 0) {
+//             sum += arr[i]
+//         }
+//     }
+//     return sum
+// }
+//
+// const positiveSum1 = (arr) => arr.reduce((sum, n) => n > 0 ? sum + n : sum, 0)
+//
+// console.log(positiveSum([1, -2, 3, 4, 5]))
+
+/* Task 23
+Write a function which converts the input string to uppercase.
+ */
+function makeUpperCase(str) {
+    return str.toUpperCase()
 }
 
-const positiveSum1 = (arr) => arr.reduce((sum, n) => n > 0 ? sum + n : sum, 0)
-
-console.log(positiveSum([1, -2, 3, 4, 5]))
+console.log(makeUpperCase('im a programmer'))
