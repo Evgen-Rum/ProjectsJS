42a7716ec2dbf06adcea110467d51e1011953541
Evgen-Rum <new.twunk@mail.ru>
2022-05-12 13:38:59 +0300

    Task 28

--- practice.js
+++ practice.js
@@ -373,7 +373,13 @@ You should return an array of all the anagrams or an empty array if there are no
 // console.log(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']))
 
 /* Task 28
-
+Given a positive integer n written as abcd... (a, b, c, d... being digits) and a positive integer p
+we want to find a positive integer k, if it exists,
+such that the sum of the digits of n taken to the successive powers of p is equal to k * n.
+In other words:
+Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
+If it is the case we will return k, if not return -1.
+Note: n and p will always be given as strictly positive integers.
  */
 function digPow(num, p) {
     let numArr = Array.from('' + num).map(Number)
@@ -388,4 +394,10 @@ function digPow(num, p) {
     }
 }
 
-console.log(digPow(1234, 1))
+function digPow1(num, pow) {
+    let x = String(num).split('').reduce((pV, cV, index ) =>
+        pV + Math.pow(cV, pow + index), 0)
+    return x % num ? -1 : x / num
+}
+
+console.log(digPow1(89, 1))
